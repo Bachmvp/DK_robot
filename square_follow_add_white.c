3e383f41c24fc42ceeb629c5cb029751a2a4a296
Bachmvp <1622421595@qq.com>
2020-01-16 23:10:20 +0100

    debug

--- square_follow_add_white.c
+++ square_follow_add_white.c
@@ -304,7 +304,7 @@ int main()
         if (lmssrv.connected) {
             xmllaser = xml_in_init(4096, 32);
             printf(" laserserver xml initialized \n");
-            len=sprintf(buf,”scanpush cmd='zoneobst'\n”);
+            len=sprintf(buf,"scanpush cmd='zoneobst'\n");
             send(lmssrv.sockfd, buf, len, 0);
         }
 
